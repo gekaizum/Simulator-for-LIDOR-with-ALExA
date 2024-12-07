3ad665c5abf8debf53c8f20e86fab58be3b0c5a4
Yevgeniy Gluhoy <128051209+gekaizum@users.noreply.github.com>
2024-12-07 17:27:31 +0200

    map generator
    Random generator improved

--- Simulator_code/Physical_layer/Map_block/python_map_generator.py
+++ Simulator_code/Physical_layer/Map_block/python_map_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as rd
 from scipy.ndimage import gaussian_filter
 import matplotlib.pyplot as plt
 
@@ -7,7 +8,7 @@ grid_size = 5000  # 5000x5000 meters
 resolution = 1    # Resolution of 1x1 meter
 
 # Generate terrain heights
-np.random.seed(42)  # For reproducibility
+np.random.seed(rd.randint(1,100))  # For reproducibility
 terrain = np.random.rand(grid_size, grid_size) * 300  # Heights from 0 to 100 meters
 terrain = gaussian_filter(terrain, sigma=50)  # Smoothing for a natural appearance
 
@@ -21,7 +22,7 @@ np.savetxt("heightmap_cm.txt", terrain_cm, fmt="%d")  # Save as integers
 plt.figure(figsize=(20, 20))  # Adjust the figure size for high resolution
 plt.imshow(terrain_cm, cmap="terrain")
 plt.colorbar(label="Height (cm)")
-plt.title("Heightmap (in centimeters)", fontsize=20)
+plt.title("Heightmap (in cantimeters)", fontsize=20)
 plt.axis('off')  # Optional: turn off axes for a cleaner look
 
 # Save the image in high resolution
