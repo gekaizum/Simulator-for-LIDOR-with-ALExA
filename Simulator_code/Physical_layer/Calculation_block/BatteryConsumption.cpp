97f617e9b1cf0cef4f3c622f25dff2509bedcc1d
Yevgeniy Gluhoy <128051209+gekaizum@users.noreply.github.com>
2025-01-26 23:44:15 +0200

    Add files via upload
    Multiple updates and optimization of the code

--- Simulator_code/Physical_layer/Calculation_block/BatteryConsumption.cpp
+++ Simulator_code/Physical_layer/Calculation_block/BatteryConsumption.cpp
@@ -1,53 +1,48 @@
 #include <omnetpp.h>
+#include <iostream>
 
 using namespace omnetpp;
 
-//Define_Module(BatteryConsumption);
+//weight - total weight of drone [kg]
+//num_rotors - number of rotors
+//motor_efficiency - efficiency of motors [W/N]
+//E_hover = P_hover * t[sec]
 
-// Get parameters from message or module parameters
-//    double weight = par("weight");                  // [kg]
-//    double gravity = par("gravity");                // [m/s^2]
-//    double voltage = par("voltage");                // [V]
-//    double efficiency = par("efficiency");          // [0-1]
-//    double acceleration = par("acceleration");      // [m/s^2]
-//    double velocity = par("velocity");              // [m/s]
-//    double antennaPower = par("antennaPower");      // [W]
-//    double sensorPower = par("sensorPower");        // [W]
-//    double additionalPower = par("additionalPower");// [W]
-//    double timeStep = par("timeStep");              // [s]
-
-    // Step 1: Calculate hovering current (computed once if static)
-//    double hoveringCurrent = calculateHoveringCurrent(weight, gravity, voltage, efficiency);
-
-    // Step 2: Calculate total current consumption
-//    double currentConsumption = calculateTotalCurrent(hoveringCurrent, acceleration, velocity, antennaPower, sensorPower, additionalPower);
-
-    // Step 3: Update remaining battery capacity
-//    updateBatteryCapacity(remainingCapacity, currentConsumption, timeStep);
-
-    // Log remaining capacity
-//   EV << "Remaining battery capacity: " << remainingCapacity << " mAh" << endl;
-
-    // Clean up message
-//    delete msg;
-//}
-
-// Function to calculate hovering current
-inline double calculateHoveringCurrent(double weight, double voltage, double efficiency) {
-    double gravity = 9.8;
-    return (weight * gravity) / (voltage * efficiency);
+// Function to calculate power needed for hovering
+inline double calculateHoveringPower(float weight, float motor_efficiency) {
+    float gravity = 9.81; //[m/sec^2]
+	float P_hover = (weight * gravity) / motor_efficiency;
+    return P_hover;
 }
 
 // Function to calculate total current consumption
-inline double calculateTotalCurrent(double hoveringCurrent, double acceleration, double velocity, double antennaPower, double sensorPower, double additionalPower) {
-    double accelerationCurrent = acceleration / 10.0; // Example scale factor
-    double antennaCurrent = antennaPower / 11.1; // Based on battery voltage [V]
-    double sensorCurrent = sensorPower / 11.1;  // Based on battery voltage [V]
-    return hoveringCurrent + accelerationCurrent + antennaCurrent + sensorCurrent + additionalPower;
+// Will be used after any movement command or sending message
+inline double calculateTotalCurrent(float weight, double acceleration, double velocity) {
+	double accelerationEnergy =0;
+	double velocityEnergy =0;
+	if (acceleration != 0){ //will be done once when move command received
+		accelerationEnergy = 0.5 * weight * std::pow(acceleration, 2)) * (velocity/acceleration);
+	}
+	if (velocity != 0) {//will be done once when move command received
+		double air_resistance_factor = 0.05; // Adjusted based on aerodynamics
+		double drag_power = air_resistance_factor * std::pow(velocity, 2);
+		velocityEnergy = drag_power * (velocity/acceleration); // Energy in Joules
+	}
+	double totalCurrentConsumption = accelerationEnergy + velocityEnergy;
+    return totalCurrentConsumption;
 }
 
 // Function to update remaining battery capacity
-inline double updateBatteryCapacity(double &remainingCapacity, double currentConsumption, double timeStep) {
-    remainingCapacity -= (currentConsumption * timeStep * 1000) / 3600.0;
+// sensorPower + hoveringPower  - will be calculated periodically for each drone
+// user algorithm will use it when it has additional power needs
+// remainingCapacity - Remaining battery capacity in Joules (J)
+inline double updateBatteryCapacity(double &remainingCapacity, double currentConsumption, bool flag, 
+									double sensorPower, double additionalPower, double hoveringPower, int time_step) {
+	if (flag == true){// need to be calculated periodically for each drone
+		remainingCapacity = remainingCapacity - (sensorPower + hoveringPower + additionalPower)*time_step;
+	}
+	else { //Will be used after any movement command or sending message
+		remainingCapacity -= currentConsumption
+	}
     return remainingCapacity;
 }
--- Simulator_code/Physical_layer/Calculation_block/SignalStrengthCalculation.cpp
+++ Simulator_code/Physical_layer/Calculation_block/SignalStrengthCalculation.cpp
@@ -1,115 +1,82 @@
 #include <omnetpp.h>
 #include <cmath>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <sstream>
 
 using namespace omnetpp;
 
-/**
+/*
  * The SignalStrengthCalculation class computes signal strength based on various propagation models,
  * including Free-Space Path Loss, Two-Ray Ground Reflection, and Knife-Edge Diffraction. It also
  * accounts for terrain data to calculate effective obstacle heights for the Knife-Edge model.
  */
-/*
-class SignalStrengthCalculation : public cSimpleModule {
-  private:
-    double calculateFreeSpacePathLoss(double transmittedPower, double gainTransmitter, double gainReceiver, double distance, double frequency);
-    double calculateTwoRayGroundReflection(double transmittedPower, double gainTransmitter, double gainReceiver, double h_Transmitter, double h_Receiver, double distance);
-    double calculateKnifeEdgeDiffraction(double transmittedPower, double wavelength, double t_to_obst, double r_to_obst, double h_Eff);
-    double calculateEffectiveHeight(double t_to_obst, double r_to_obst, double h_Transmitter, double h_Receiver, double h_obstacle);
-  protected:
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
+ 
+ 
+ // First you need to calculate signal loss using calculateSignalLoss()
+ // Then received power in dBm using receivedPower()
+ 
+const double SPEED_OF_LIGHT = 3e8;  // Speed of light in m/s
+const double PI = 3.141592653589793;
+
+// Enum for selecting the propagation model
+enum class PropagationModel {
+    FREE_SPACE,
+    TWO_RAY,
+    KNIFE_EDGE
 };
-*/
-//Define_Module(SignalStrengthCalculation);
-/*
-void SignalStrengthCalculation::initialize() {
-    // Initialization logic if required
+
+// Function to compute received power in dBm
+double receivedPower(double transmittedPower, double gainTx, double gainRx, double pathLoss) {
+    return transmittedPower + gainTx + gainRx - pathLoss;
 }
 
-void SignalStrengthCalculation::handleMessage(cMessage *msg) {
-    // Extract parameters from the message or module
-    std::string propagationModel = par("propagationModel");
-    std::string communicationType = par("communicationType");
-    double transmittedPower = par("transmittedPower"); // [W]
-    double gainTransmitter = par("gainTransmitter");   // [linear scale]
-    double gainReceiver = par("gainReceiver");         // [linear scale]
-    double distance = par("distance");                // [m]
-    double frequency = par("frequency");              // [Hz]
-    double h_Transmitter = par("h_Transmitter"); // [m]
-    double h_Receiver = par("h_Receiver");       // [m]
-    double t_to_obst = par("t_to_obst"); // Distance to obstacle from transmitter [m]
-    double r_to_obst = par("r_to_obst"); // Distance to obstacle from receiver [m]
-    double h_obstacle = par("h_obstacle"); // max height [m] in the map along the line of sight
+// Function to calculate Free-Space Path Loss (FSPL)
+double calculateFreeSpacePathLoss(double frequencyHz, double distanceMeters) {
+    if (distanceMeters <= 0) return 0;
+    double wavelength = SPEED_OF_LIGHT / frequencyHz;
+    return 20 * log10(distanceMeters) + 20 * log10(frequencyHz) - 147.55;  // dB
+}
 
-/*
-    // Coordinates for terrain-based height calculation
-    double transmitterX = par("transmitterX");
-    double transmitterY = par("transmitterY");
-    double receiverX = par("receiverX");
-    double receiverY = par("receiverY");
-*/    
-/*
+// Function to calculate Two-Ray Ground Reflection
+double calculateTwoRayGroundReflection(double txHeight, double rxHeight, double distance, double frequencyHz) {
+    if (distance <= 0) return 0;
+    double directPathLoss = freeSpacePathLoss(frequencyHz, distance);
+    double groundReflectionLoss = 10 * log10((txHeight * rxHeight) / (distance * distance));
+    return directPathLoss - groundReflectionLoss;
+}
 
-    double h_Eff = calculateEffectiveHeight(t_to_obst, r_to_obst, h_Transmitter, h_Receiver, h_obstacle);
+// Function to calculate Knife-Edge Diffraction
+double calculateKnifeEdgeDiffraction(double obstacleHeight, double t_to_obst, double r_to_obst, double frequencyHz) {
+	double wavelength = SPEED_OF_LIGHT/frequencyHz;
+	// Calculate the diffraction parameter v
+    double v = (sqrt(2) * obstacleHeight) / sqrt((wavelength * (t_to_obst + r_to_obst)) / (t_to_obst * r_to_obst));
+    if (v <= -0.78) return 0;   // If v <= -0.78, there is no significant attenuation
+	// Compute the diffraction loss (in dB)
+    double diffractionLoss = 6.9 + 20 * log10(sqrt((v - 0.1) * (v - 0.1) + 1) + v - 0.1);
+    return diffractionLoss;
+}
 
-    double signalStrength = 0.0;
+// Unified function to compute signal loss based on the selected model
+double calculateSignalLoss(PropagationModel model, double frequencyHz, double distance, 
+                           double txHeight = 0, double rxHeight = 0, 
+                           double obstacleHeight = 0, double t_to_obst = 0, double r_to_obst = 0) {
+    double pathLoss = 0;
 
-    if (propagationModel == "FreeSpace") {
-        signalStrength = calculateFreeSpacePathLoss(transmittedPower, gainTransmitter, gainReceiver, distance, frequency);
-    } else if (propagationModel == "TwoRay") {
-        signalStrength = calculateTwoRayGroundReflection(transmittedPower, gainTransmitter, gainReceiver, h_Transmitter, h_Receiver, distance);
-    } else if (propagationModel == "KnifeEdge") {
-        double wavelength = 3e8 / frequency; // Speed of light / frequency
-        signalStrength = calculateKnifeEdgeDiffraction(transmittedPower, wavelength, t_to_obst, r_to_obst, h_Eff);
+    switch (model) {
+        case PropagationModel::FREE_SPACE:
+            pathLoss = freeSpacePathLoss(frequencyHz, distance);
+            break;
+        case PropagationModel::TWO_RAY:
+            pathLoss = twoRayGroundReflection(txHeight, rxHeight, distance, frequencyHz);
+            break;
+        case PropagationModel::KNIFE_EDGE:
+            pathLoss = knifeEdgeDiffraction(obstacleHeight, t_to_obst, r_to_obst, frequencyHz);
+            break;
+        default:
+            std::cerr << "Invalid propagation model selected!" << std::endl;
+            return -1;
     }
-
-    EV << "Calculated Signal Strength: " << signalStrength << " W" << endl;
-
-    delete msg;
-}
-*/
-
-inline double calculateFreeSpacePathLoss(double transmittedPower, double gainTransmitter, double gainReceiver, double distance, double frequency) {
-    double c = 3e8; // Speed of light [m/s]
-    double p_r = transmittedPower + gainTransmitter + gainReceiver -20*log10(distance) - 20*log10(frequency) - 20*log10(4*M_PI/c);
-    return p_r;
-}
-
-inline double calculateTwoRayGroundReflection(double transmittedPower, double gainTransmitter, double gainReceiver, double h_Transmitter, double h_Receiver, double distance) {
-    double p_r = transmittedPower - 40*log10(distance) + 10*log10(gainTransmitter*gainReceiver*pow(h_Receiver*h_Transmitter,2));
-    return p_r;
-}
-
-inline double calculateKnifeEdgeDiffraction(double transmittedPower, double wavelength, double t_to_obst, double r_to_obst, double h_Eff) {
-    double v = (h_Eff * sqrt(2 * (t_to_obst + r_to_obst) / (wavelength * t_to_obst * r_to_obst)));
-    double p_r = transmittedPower - 6.9 -20 * log10(sqrt(pow((v - 0.1),2) + 1)) + v - 0.1;
-    return p_r;
-}
-
-inline double calculateEffectiveHeight(double t_to_obst, double r_to_obst, double h_Transmitter, double h_Receiver, double h_obstacle) {
-    double h_Eff = h_obstacle - (h_Transmitter + t_to_obst * (h_Receiver - h_Transmitter) / (t_to_obst + r_to_obst));
-    return h_Eff;
-}
-
-//{FSPL=Free Space path loss, TRGR=Two ray ground reflection, KED=Knife edge diffraction}
-inline double checkSignalStrength(char *propagationModel,double transmittedPower,double gainTransmitter, double gainReceiver, double distance, double frequency,
-        double h_Transmitter, double h_Receiver, double t_to_obst, double r_to_obst, double h_obstacle) {
-    double result=0;
-    if (strcmp(propagationModel, "FSPL") == 0){
-        result = calculateFreeSpacePathLoss(transmittedPower, gainTransmitter, gainReceiver, distance, frequency);
-    }
-    else if (strcmp(propagationModel, "TRGR") == 0){
-        result = calculateTwoRayGroundReflection(transmittedPower, gainTransmitter, gainReceiver, h_Transmitter, h_Receiver, distance);
-    }
-    else if (strcmp(propagationModel, "KED") == 0){
-        const double c = 3.0e8;  // Speed of light in m/s
-        double wavelength = c/frequency;
-        //double h_obstacle=HeightMapLoader::findMaxHeightOnLine(x1, y1, x2, y2);
-        double h_Eff=calculateEffectiveHeight(t_to_obst, r_to_obst, h_Transmitter, h_Receiver, h_obstacle);
-        result = calculateKnifeEdgeDiffraction(transmittedPower, wavelength, t_to_obst, r_to_obst, h_Eff);
-    }
-    return result;
+    return pathLoss;
 }
