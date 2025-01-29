36fb5f174ccaac6f2f945c065c5644eda99ed0b3
Yevgeniy Gluhoy <128051209+gekaizum@users.noreply.github.com>
2025-01-29 16:25:22 +0200

    Add files via upload

--- Simulator_code/Physical_layer/Calculation_block/BatteryConsumption.cpp
+++ Simulator_code/Physical_layer/Calculation_block/BatteryConsumption.cpp
@@ -1,77 +1,48 @@
-#include <omnetpp.h>
-
+//#include <omnetpp.h>
+//#include <iostream>
+#include "BatteryConsumption.h"
 using namespace omnetpp;
 
-class BatteryConsumption : public cSimpleModule {
-  private:
-    double remainingCapacity; // [mAh]
+//weight - total weight of drone [kg]
+//num_rotors - number of rotors
+//motor_efficiency - efficiency of motors [W/N]
+//E_hover = P_hover * t[sec]
 
-  protected:
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
-
-    // Function to calculate hovering current
-    double calculateHoveringCurrent(double weight, double gravity, double voltage, double efficiency);
-
-    // Function to calculate total current consumption
-    double calculateTotalCurrent(double hoveringCurrent, double acceleration, double velocity, double antennaPower, double sensorPower, double additionalPower);
-
-    // Function to update remaining battery capacity
-    void updateBatteryCapacity(double &remainingCapacity, double currentConsumption, double timeStep);
-};
-
-Define_Module(BatteryConsumption);
-
-// Initialize module parameters
-void BatteryConsumption::initialize() {
-    // Get initial battery capacity from NED parameters
-    remainingCapacity = par("initialBatteryCapacity"); // [mAh]
-}
-
-// Handle incoming messages (e.g., simulation steps)
-void BatteryConsumption::handleMessage(cMessage *msg) {
-    // Get parameters from message or module parameters
-    double weight = par("weight");                  // [kg]
-    double gravity = par("gravity");                // [m/s^2]
-    double voltage = par("voltage");                // [V]
-    double efficiency = par("efficiency");          // [0-1]
-    double acceleration = par("acceleration");      // [m/s^2]
-    double velocity = par("velocity");              // [m/s]
-    double antennaPower = par("antennaPower");      // [W]
-    double sensorPower = par("sensorPower");        // [W]
-    double additionalPower = par("additionalPower");// [W]
-    double timeStep = par("timeStep");              // [s]
-
-    // Step 1: Calculate hovering current (computed once if static)
-    double hoveringCurrent = calculateHoveringCurrent(weight, gravity, voltage, efficiency);
-
-    // Step 2: Calculate total current consumption
-    double currentConsumption = calculateTotalCurrent(hoveringCurrent, acceleration, velocity, antennaPower, sensorPower, additionalPower);
-
-    // Step 3: Update remaining battery capacity
-    updateBatteryCapacity(remainingCapacity, currentConsumption, timeStep);
-
-    // Log remaining capacity
-    EV << "Remaining battery capacity: " << remainingCapacity << " mAh" << endl;
-
-    // Clean up message
-    delete msg;
-}
-
-// Function to calculate hovering current
-double BatteryConsumption::calculateHoveringCurrent(double weight, double gravity, double voltage, double efficiency) {
-    return (weight * gravity) / (voltage * efficiency);
+// Function to calculate power needed for hovering
+double calculateHoveringPower(double weight, double motor_efficiency) {
+    double gravity = 9.81; //[m/sec^2]
+    double P_hover = (weight * gravity) / motor_efficiency;
+    return P_hover;
 }
 
 // Function to calculate total current consumption
-double BatteryConsumption::calculateTotalCurrent(double hoveringCurrent, double acceleration, double velocity, double antennaPower, double sensorPower, double additionalPower) {
-    double accelerationCurrent = acceleration / 10.0; // Example scale factor
-    double antennaCurrent = antennaPower / 11.1; // Based on battery voltage [V]
-    double sensorCurrent = sensorPower / 11.1;  // Based on battery voltage [V]
-    return hoveringCurrent + accelerationCurrent + antennaCurrent + sensorCurrent + additionalPower;
+// Will be used after any movement command or sending message
+double calculateTotalCurrent(double weight, double acceleration, double velocity) {
+	double accelerationEnergy =0;
+	double velocityEnergy =0;
+	if (acceleration != 0){ //will be done once when move command received
+		accelerationEnergy = 0.5 * weight * std::pow(acceleration, 2) * (velocity/acceleration);
+	}
+	if (velocity != 0) {//will be done once when move command received
+		double air_resistance_factor = 0.05; // Adjusted based on aerodynamics
+		double drag_power = air_resistance_factor * std::pow(velocity, 2);
+		velocityEnergy = drag_power * (velocity/acceleration); // Energy in Joules
+	}
+	double totalCurrentConsumption = accelerationEnergy + velocityEnergy;
+    return totalCurrentConsumption;
 }
 
 // Function to update remaining battery capacity
-void BatteryConsumption::updateBatteryCapacity(double &remainingCapacity, double currentConsumption, double timeStep) {
-    remainingCapacity -= (currentConsumption * timeStep * 1000) / 3600.0;
+// sensorPower + hoveringPower  - will be calculated periodically for each drone
+// user algorithm will use it when it has additional power needs
+// remainingCapacity - Remaining battery capacity in Joules (J)
+double updateBatteryCapacity(double &remainingCapacity, double currentConsumption, bool flag,
+									double sensorPower, double additionalPower, double hoveringPower, int time_step) {
+	if (flag == true){// need to be calculated periodically for each drone
+		remainingCapacity = remainingCapacity - (sensorPower + hoveringPower + additionalPower)*time_step;
+	}
+	else { //Will be used after any movement command or sending message
+		remainingCapacity -= currentConsumption;
+	}
+    return remainingCapacity;
 }
