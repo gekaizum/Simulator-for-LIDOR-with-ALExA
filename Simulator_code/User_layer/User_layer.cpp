a8e58565e3f2a7539f5a9b19e6a998c978a179bc
Yevgeniy Gluhoy <128051209+gekaizum@users.noreply.github.com>
2025-04-09 13:41:11 +0300

    Add files via upload

--- Simulator_code/User_layer/User_layer.cpp
+++ Simulator_code/User_layer/User_layer.cpp
@@ -7,74 +7,52 @@
 
 #include "User_layer.h"
 
-void UserLayer::initialize() { // Initializes the simulation and get access to basic protocol module
+Define_Module(UserLayer);
+
+void UserLayer::initialize() { // Initializes the drone module
+//    UserLayer_Logger = new UserLayerLogger();
+//    UserLayer_Logger->logFile.flush();
+    //controlModule = check_and_cast<SimulationControl*>(getParentModule()->getSubmodule("simControl"));
     basicProtocolModule = check_and_cast<BasicProtocol*>(getParentModule()->getSubmodule("basicProtocol"));
-    // create and send Start self cMessage to start the simulation
-    cMessage *startMsg = new cMessage("startMsg");
-    startMsg->addPar("startMsg") = "startMsg";
-    scheduleAt(simTime()+1, startMsg);
+//    UserLayer_Logger->logFile << "User layer online" << endl;
+    testMsg = new cMessage("testMsg");
+    testMsg->addPar("State") = "testMsg";
+    scheduleAt(simTime()+2, testMsg);
 }
 
 void UserLayer::handleMessage(cMessage *msg){ // Handles incoming messages
-        if(strcmp(msg->par("startMsg").stringValue(), "startMsg") == 0){
-            // Power on the drones
-            basicProtocolModule->powerOn_drone(1, 0, 0, 0);
-            basicProtocolModule->powerOn_drone(2, 0, 0, 0);
-            basicProtocolModule->powerOn_drone(3, 0, 0, 0);
-            basicProtocolModule->set_velocity(1, 1, 0, 0, 10); // set Drone1 velocity
-            basicProtocolModule->set_acceleration(1, 1, 5); //set Drone1 acceleration
-            basicProtocolModule->take_off(1, 1, 0, 0, 300); // Drone1 takes off to 300m altitude
+        if(msg == testMsg){
+            // Power on drones 1 and 2
+
+            basicProtocolModule->height_checker(0, 0, z_val);
+            basicProtocolModule->powerOn_drone(1, 0, 0, z_val);
+            basicProtocolModule->powerOn_drone(2, 0, 0, z_val);
+            basicProtocolModule->powerOn_drone(3, 0, 0, z_val);
+            //basicProtocolModule->powerOn_drone(4, 0, 0, 0);
+
+            // Take off Drone 1
+            basicProtocolModule->set_velocity(1, 1,1, 0, 0, 10);
+            basicProtocolModule->set_acceleration(1,1, 1, 5);
+            basicProtocolModule->take_off(1, 1,1, 0, 0, z_val+50);      // Drone 1 takes off to 10m altitude
+            //basicProtocolModule->take_off(1, 1, 150, 0, 10);     // Move Drone 1 forward until it reaches 150 meters
             cout << "Drone 1 took off and started moving...\n";
-
-            simMsg->addPar("simMsg") = "simMsg";// create and send Simulation self cMessage
-            scheduleAt(simTime()+10, simMsg);
+            Start_Flag = false;
+            moveEV = new cMessage("moveTest");
+            scheduleAt(simTime()+10, moveEV);
         }
-        if(strcmp(msg->par("simMsg").stringValue(), "simMsg") == 0){ //handles simulation cMessages
-            basicProtocolModule->sendMsgTCP(2, 1); // TCP general data message
-            basicProtocolModule->getDrone_position(1, 1, pos_array_1); //update the position array of Drone1
-            basicProtocolModule->getDrone_position(1, 1, pos_array_3); //update the position array of Drone3 (the relay drone)
-
-            if(pos_array_1[0] == 0 && pos_array_1[1] == 0 && pos_array_1[2] == 300){ //check if Drone1 is at z = 300m to start moving forward
-                basicProtocolModule->set_velocity(1, 1, 5, 0, 0);
-                basicProtocolModule->set_acceleration(1, 1, 5); //set Drone1 acceleration
-                basicProtocolModule->move_to(1, 1, 1000, 0, 300);   // Drone 1 start moving to the destination
-            }
-            if (pos_array_1[0] >= 250 && pos_array_3[0] == 0) { // Check if Drone 3 should take off as a relay
-                basicProtocolModule->sendMsgTCP(1, 3); // TCP command message to drone 3 to start moving to {500,0,300}
-
-                //simulates sending a cMessage from Drone3 to User_layer
-                cMessage *Drone3Msg = new cMessage("Drone3Msg");
-                Drone3Msg->addPar("Drone3Msg") = "Drone3Msg";
-                scheduleAt(simTime()+1, Drone3Msg);
-
-            }
-            if(pos_array_3[2] == 300 && relayFlag == true){
-                basicProtocolModule->set_velocity(1, 3, 10, 0, 0); //set Drone3 velocity
-                basicProtocolModule->set_acceleration(1, 3, 5); //set Drone3 acceleration
-                basicProtocolModule->move_to(1, 3, 500, 0, 300);
-                relayFlag = false;
-                scheduleAt(simTime()+1, simMsg);
-            }
-            if(pos_array_1[0] == 1000){
-                // Stop Drone1 when it reaches 1000 meters
-                basicProtocolModule->stop_drone(1, 1);
-                cout << "Drone 1 reached 1000 meters and stopped moving.\n";
-                cMessage *simEndMsg = new cMessage("simEndMsg");
-                simEndMsg->addPar("simEndMsg") = "simEndMsg";
-                scheduleAt(simTime()+1, simEndMsg);
-            }
+        else if(msg == moveEV){
+            basicProtocolModule->set_velocity(1, 1,1, 5, 5, 5);
+            basicProtocolModule->set_acceleration(1,1, 1, 1);
+            basicProtocolModule->move_to(1,1, 1,50, 50, z_val-100);
+            testMsg2 = new cMessage("moveTest");
+            scheduleAt(simTime()+10, testMsg2);
         }
-        if(strcmp(msg->par("Drone3Msg").stringValue(), "Drone3Msg") == 0){ //handles cMessages form drone3
-            basicProtocolModule->set_velocity(1, 3, 0, 0, 10); // set Drone3 velocity
-            basicProtocolModule->set_acceleration(1, 3, 5); //set Drone3 acceleration
-            basicProtocolModule->take_off(1, 3, 0, 0, 300); // Drone3 takes off to 300m altitude
-            cout << "Drone3 took off as a relay\n";
-        }
-        if(strcmp(msg->par("simEndMsg").stringValue(), "simEndMsg") == 0){ //handles End of simulation cMessage
-            basicProtocolModule->sendMsgTCP(1, 2); // Drone1 informs Drone2 via a TCP message that it has reached its destination
-            /*
-             * clean memory and close files here if needed
-             */
-            cout << "End of Simulation\n";
+        else{
+            basicProtocolModule->set_velocity(1, 2,2, 5, 5, 5);
+            basicProtocolModule->set_acceleration(1,2, 2, 1);
+            basicProtocolModule->take_off(3, 1,2, 0, 0, z_val+50);      // Drone 1 takes off to 10m altitude
+
+            cout << "Drone 1 reached 150 meters and stopped moving.\n";
         }
 }
+
