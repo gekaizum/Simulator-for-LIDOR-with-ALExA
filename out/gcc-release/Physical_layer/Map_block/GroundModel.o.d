884e5e571d36c8f48726c2957f25513bc81c8205
Shaked Basa <128978272+ShakedBasa@users.noreply.github.com>
2025-06-08 00:20:37 +0300

    Add files via upload

---
+++ out/gcc-release/Physical_layer/Map_block/GroundModel.o.d
@@ -0,0 +1,25 @@
+out/gcc-release//Physical_layer/Map_block/GroundModel.o: \
+ Physical_layer/Map_block/GroundModel.cpp \
+ Physical_layer/Map_block/GroundModel.h \
+ ../inet/src/inet/environment/contract/IGround.h \
+ ../inet/src/inet/common/geometry/common/Coord.h \
+ ../inet/src/inet/common/INETMath.h ../inet/src/inet/common/INETDefs.h \
+ ../inet/src/inet/common/precompiled_release.h \
+ ../inet/src/inet/common/precompiled.h \
+ /home/shaked/omnetpp-6.1/include/omnetpp.h \
+ ../inet/src/inet/opp_defines.h ../inet/src/inet/features.h \
+ ../inet/src/inet/common/Compat.h ../inet/src/inet/common/InitStages.h \
+ ../inet/src/inet/common/InitStageRegistry.h
+Physical_layer/Map_block/GroundModel.h:
+../inet/src/inet/environment/contract/IGround.h:
+../inet/src/inet/common/geometry/common/Coord.h:
+../inet/src/inet/common/INETMath.h:
+../inet/src/inet/common/INETDefs.h:
+../inet/src/inet/common/precompiled_release.h:
+../inet/src/inet/common/precompiled.h:
+/home/shaked/omnetpp-6.1/include/omnetpp.h:
+../inet/src/inet/opp_defines.h:
+../inet/src/inet/features.h:
+../inet/src/inet/common/Compat.h:
+../inet/src/inet/common/InitStages.h:
+../inet/src/inet/common/InitStageRegistry.h:
---
+++ out/gcc-release/Physical_layer/Map_block/TerrainAwareSuiPathLoss.o.d
@@ -0,0 +1,29 @@
+out/gcc-release//Physical_layer/Map_block/TerrainAwareSuiPathLoss.o: \
+ Physical_layer/Map_block/TerrainAwareSuiPathLoss.cpp \
+ Physical_layer/Map_block/TerrainAwareSuiPathLoss.h \
+ Physical_layer/Map_block/GroundModel.h \
+ ../inet/src/inet/environment/contract/IGround.h \
+ ../inet/src/inet/common/geometry/common/Coord.h \
+ ../inet/src/inet/common/INETMath.h ../inet/src/inet/common/INETDefs.h \
+ ../inet/src/inet/common/precompiled_release.h \
+ ../inet/src/inet/common/precompiled.h \
+ /home/shaked/omnetpp-6.1/include/omnetpp.h \
+ ../inet/src/inet/opp_defines.h ../inet/src/inet/features.h \
+ ../inet/src/inet/common/Compat.h ../inet/src/inet/common/InitStages.h \
+ ../inet/src/inet/common/InitStageRegistry.h \
+ ../inet/src/inet/common/ModuleAccess.h
+Physical_layer/Map_block/TerrainAwareSuiPathLoss.h:
+Physical_layer/Map_block/GroundModel.h:
+../inet/src/inet/environment/contract/IGround.h:
+../inet/src/inet/common/geometry/common/Coord.h:
+../inet/src/inet/common/INETMath.h:
+../inet/src/inet/common/INETDefs.h:
+../inet/src/inet/common/precompiled_release.h:
+../inet/src/inet/common/precompiled.h:
+/home/shaked/omnetpp-6.1/include/omnetpp.h:
+../inet/src/inet/opp_defines.h:
+../inet/src/inet/features.h:
+../inet/src/inet/common/Compat.h:
+../inet/src/inet/common/InitStages.h:
+../inet/src/inet/common/InitStageRegistry.h:
+../inet/src/inet/common/ModuleAccess.h:
